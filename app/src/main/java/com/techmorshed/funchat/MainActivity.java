dbaf3713feffe0f1b981861f872b406e6204ae93
Morshed <morshedislam21@gmail.com>
2019-04-13 21:11:37 +0600

    Initial commit

---
+++ app/src/main/java/com/techmorshed/funchat/MainActivity.java
@@ -0,0 +1,86 @@
+package com.techmorshed.funchat;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.techmorshed.funchat.activity.StartActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser == null){
+
+            sendToStart();
+
+        } else {
+
+//            mUserRef.child("online").setValue("true");
+
+        }
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.main_logout_btn){
+            FirebaseAuth.getInstance().signOut();
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void sendToStart() {
+
+        Intent startIntent = new Intent(MainActivity.this, StartActivity.class);
+        startActivity(startIntent);
+        finish();
+
+    }
+}
